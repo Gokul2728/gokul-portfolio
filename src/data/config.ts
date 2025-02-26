fabcbfd182f491dbbd639fbc53ae518e57118771
Gokul2728 <gokulat8@gmail.com>
2025-02-26 14:50:35 +0530

    Ghost Rider

---
+++ src/data/config.ts
@@ -0,0 +1,45 @@
+const config = {
+  title: "Gokul Ponnusamy | FullStack Developer",
+  description: {
+    long: "Explore the portfolio of Gokul, a Computer Science Engineer with 2.5 year experience in Devlopment, DevOps & Database management with hands-on experience in deploying & automating workflows. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    short:
+      "Discover the portfolio of Gokul, a FullStack Developer solving real life problems in DevOps and Development.",
+  },
+  keywords: [
+    "Gokul",
+    "Gokul portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    "AI Docker file optimizer",
+    // "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    // "AIOps",
+    "Azure",
+  ],
+  author: "Gokul Ponnusamy",
+  email: "gokulponnusamy02@gmail.com",
+  site: "https://abhijitzende.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/_Abhi_Speaks/",
+    linkedin: "https://www.linkedin.com/in/zende-abhijit/",
+    instagram: "https://www.instagram.com/_.celestial_abhi._/",
+    facebook: "https://www.facebook.com/profile.php?id=100010029821759",
+    github: "https://github.com/Abhiz2411/",
+  },
+};
+export { config };
